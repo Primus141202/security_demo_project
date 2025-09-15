0bfdd23f394d9a122ab3e1658b43773d361df5e9
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-15 16:02:21 +0530

    Initial commit

---
+++ src/main/java/com/sprk/security_demo_project/SecurityDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sprk.security_demo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprk/security_demo_project/SecurityDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprk.security_demo_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
