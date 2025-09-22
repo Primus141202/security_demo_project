11f6bd4471fa22032d5bd566fbc1d5c649cf4a98
Primus141202 <108917380+Primus141202@users.noreply.github.com>
2025-09-22 14:12:34 +0530

    Update MainController.java

--- src/main/java/com/sprk/security_demo_project/controller/MainController.java
+++ src/main/java/com/sprk/security_demo_project/controller/MainController.java
@@ -1,10 +1,19 @@
 package com.sprk.security_demo_project.controller;
 
+import com.sprk.security_demo_project.entity.UserInfo;
+import com.sprk.security_demo_project.service.UserInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class MainController {
+
+    private final UserInfoService userInfoService;
 
     @GetMapping("/test")
     public String test() {
@@ -14,5 +23,29 @@ public class MainController {
     public String home() {
         return "Home Page after giving correct username and password security";
     }
-}
 
+    @GetMapping("/tester/test")
+    @PreAuthorize("hasAuthority('ROLE_TESTER')")
+    public String tester() {
+        return "testing security I am having Tester Role";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public String showUser() {
+        return "Testing security I am having User Role";
+    }
+
+    @GetMapping("/admin/show-admin")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String showAdmin() {
+        return "I am admin";
+    }
+
+    @PostMapping("/signup")
+    public UserInfo signup(@RequestBody UserInfo userInfo) {
+        UserInfo signUpUserInfo = userInfoService.signUpUser(userInfo);
+
+        return signUpUserInfo;
+    }
+}
